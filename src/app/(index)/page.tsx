03c390237b3a9153607457c8d85f46c763a2e4fc
Aine <aaluowan@gmail.com>
2025-01-11 12:55:47 -0800

    added repsonsiveness to homepage

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -5,23 +5,23 @@ import Navbar from '../components/Navbar';
 export default function Home() {
     return (
         <main>
-            <div className="grid grid-cols-12 gap-x-5 py-8 mx-5">
-                <div className="col-start-1 col-span-5">
+            <section className="grid grid-cols-4 lg:grid-cols-12 gap-x-5 py-8 mx-5 lg:mt-0 mt-20">
+                <div className="col-start-1 col-span-4 lg:col-span-5">
                     <h3 className="font-neuehaasunica text-xl">
                         Alex is an interaction designer crafting unique, empathy-driven experiences through
                         storytelling.
                     </h3>
                 </div>
-                <div className="col-start-1 col-span-5 py-8">
+                <div className="col-start-1 col-span-4 lg:col-span-5 py-8 row-start-2">
                     <h3 className="font-neuehaasunica text-xl">
                         Graduating in September 2025 with a degree in Interactive Arts and Technology from Simon
                         Fraser University, currently seeking product design internships or freelance opportunities.
                     </h3>
                 </div>
                 <Navbar/>
-            </div>
+            </section>
 
-            <div className="grid grid-cols-12 gap-x-5 mt-60 mx-5">
+            <div className="grid grid-cols-4 lg:grid-cols-12 gap-x-5 mt-60 mx-5">
                 {/* First Content Block with Video */}
                 <div className="col-start-1 col-span-4">
                     <Link href="/projects/88rising">
@@ -36,7 +36,7 @@ export default function Home() {
                 </div>
 
                 {/* Second Content Block */}
-                <div className="col-start-5 col-span-4">
+                <div className="col-start-1 lg:col-start-5 col-span-4 py-5 lg:py-0">
                     <Link href="/projects/givemeyesterday">
                         <ContentItem
                             imageSrc="/img/givemeyesterday/thumbnail.png"
@@ -49,7 +49,7 @@ export default function Home() {
                 </div>
 
                 {/* Third Content Block */}
-                <div className="col-start-9 col-span-4">
+                <div className="col-start-1 lg:col-start-9 col-span-4">
                     <Link target="_blank" href="https://www.blaze.ai/">
                         <ContentItem
                             videoSrc="/video/blazeai/blazeaicontentdesigner.mp4"
